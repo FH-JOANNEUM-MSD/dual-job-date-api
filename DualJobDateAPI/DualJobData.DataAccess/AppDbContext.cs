f1389ba967931a9b798af166e8fc2a3f8467b0bf
haraldmandl <harald.mandl@edu.fh-joanneum.at>
2024-03-05 18:57:56 +0100

    Add DataAccess

---
+++ DualJobDateAPI/DualJobData.DataAccess/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace DualJobData.DataAccess
+{
+    public class AppDbContext : IDisposable//TODO DB Context
+    {
+        //TODO DbSets
+        //TODO OnConfiguring
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
