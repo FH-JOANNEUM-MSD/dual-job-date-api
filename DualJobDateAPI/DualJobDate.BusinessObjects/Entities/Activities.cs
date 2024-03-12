83de5586236f01c6a05e5265a9f4c47656ed235e
n.friehs <noah.friehs@edu.fh-joanneum.at>
2024-03-12 20:25:52 +0100

    Add Activities.cs and CompanyActivities.cs

---
+++ DualJobDateAPI/DualJobDate.BusinessObjects/Entities/Activities.cs
@@ -0,0 +1,9 @@
+using DualJobDate.BusinessObjects.Entities.Base;
+
+namespace DualJobDate.BusinessObjects.Entities 
+{
+    public class Activities : BaseAcademicProgramEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ DualJobDateAPI/DualJobDate.BusinessObjects/Entities/CompanyActivities.cs
@@ -0,0 +1,10 @@
+namespace DualJobDate.BusinessObjects.Entities
+{
+    public class CompanyActivities
+    {
+        public int ActivityId { get; set; }
+        public int CompanyId { get; set; }
+        public int Value { get; set; }
+    } 
+}
+
