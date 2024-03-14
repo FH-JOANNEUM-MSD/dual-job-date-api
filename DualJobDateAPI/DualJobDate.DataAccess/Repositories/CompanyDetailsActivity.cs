aa6ca37b0356afe317980c3a4744ce4eae85b0fa
Isabella Kainer <isabella.kainer@edu.fh-joanneum.at>
2024-03-14 01:22:32 +0100

    added repositories for all entities

---
+++ DualJobDateAPI/DualJobDate.DataAccess/Repositories/CompanyDetailsActivity.cs
@@ -0,0 +1,5 @@
+﻿using DualJobDate.BusinessObjects.Entities;
+
+namespace DualJobDate.DataAccess.Repositories;
+
+public class CompanyDetailsActivity(AppDbContext dbContext) : BaseRepository<CompanyDetails>(dbContext);
