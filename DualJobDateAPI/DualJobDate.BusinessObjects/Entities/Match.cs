914049541de2b9035b1f226e71504d724ff636ee
n.friehs <noah.friehs@edu.fh-joanneum.at>
2024-03-16 17:59:12 +0100

    extended User and renamed property in Match.cs

--- DualJobDateAPI/DualJobDate.BusinessObjects/Entities/Match.cs
+++ DualJobDateAPI/DualJobDate.BusinessObjects/Entities/Match.cs
@@ -4,7 +4,7 @@ namespace DualJobDate.BusinessObjects.Entities
 {
     public class Match : BaseEntity
     {
-        public int StudentId { get; set; }
+        public int UserId { get; set; }
         public bool? StudentMatch { get; set; }
         public int CompanyId { get; set; }
         public bool? CompanyMatch { get; set; }
