7cbb23b4c1002e3ce64c8e0ae683af3dbbf69542
Emanuel Neziraj <emanuel.neziraj@edu.fh-joanneum.at>
2024-04-08 15:19:01 +0200

    added swagger

--- DualJobDateAPI/DualJobDate.Api/Program.cs
+++ DualJobDateAPI/DualJobDate.Api/Program.cs
@@ -138,15 +138,12 @@ namespace DualJobDate.API
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
-
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DualJobDate API v1"));
-            }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DualJobDate API v1"));
+                
             app.MapControllers();
-            app.MapGet("/", () => "DualJobDate API. Following Endpoints are accessible:");
+            app.MapGet("/", () => "DualJobDate API.");
         }
     }
 }
