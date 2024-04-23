63ae35f2eacf7cf7a9da8710fb22091900645715
haraldmandl <harald.mandl@edu.fh-joanneum.at>
2024-04-23 10:03:24 +0200

    Refactor Tests

--- DualJobDateAPI/DualJobDate.Testing/UserControllerTests.cs
+++ DualJobDateAPI/DualJobDate.Testing/UserControllerTests.cs
@@ -4,6 +4,7 @@ using DualJobDate.Api.Controllers;
 using DualJobDate.BusinessObjects.Entities;
 using DualJobDate.BusinessObjects.Entities.Interface.Helper;
 using DualJobDate.BusinessObjects.Entities.Models;
+using DualJobDate.Testing.Fake;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -12,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using Xunit;
 using Xunit.Abstractions;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
@@ -20,27 +22,35 @@ namespace DualJobDate.Testing;
 public class UserControllerTests
 {
     private readonly ITestOutputHelper _testOutputHelper;
-    private Mock<UserManager<User>> mockUserManager = MockHelpers.MockUserManager<User>();
-    private Mock<SignInManager<User>> mockSignInManager = new Mock<SignInManager<User>>(new FakeUserManager(),
-        new Mock<IHttpContextAccessor>().Object,
-        new Mock<IUserClaimsPrincipalFactory<User>>().Object,
-        new Mock<IOptions<IdentityOptions>>().Object,
-        new Mock<ILogger<SignInManager<User>>>().Object,
-        new Mock<IAuthenticationSchemeProvider>().Object,
-        new Mock<IUserConfirmation<User>>().Object);
-    private Mock<IServiceProvider> mockServiceProvider = new Mock<IServiceProvider>();
-    private Mock<IMapper> mockMapper = new Mock<IMapper>();
-    private Mock<RoleManager<Role>> mockRoleManager = MockHelpers.MockRoleManager<Role>();
-    private UserController controller;
-    private Mock<IJwtAuthManager> mockAthManager = new Mock<IJwtAuthManager>(); 
+    private readonly Mock<UserManager<User>> _mockUserManager;
+    private readonly Mock<SignInManager<User>> _mockSignInManager;
+    private readonly UserController _controller;
+    private readonly Mock<IJwtAuthManager> _mockAuthenticationManager;
 
-    public UserControllerTests()
+    public UserControllerTests(ITestOutputHelper testOutputHelper)
     {
-        controller = new UserController(mockUserManager.Object, mockSignInManager.Object, mockServiceProvider.Object, mockMapper.Object, mockRoleManager.Object, mockAthManager.Object);
+        _testOutputHelper = testOutputHelper;
+        _mockUserManager = MockHelpers.MockUserManager<User>();
+        _mockSignInManager = new Mock<SignInManager<User>>(new FakeUserManager(),
+            new Mock<IHttpContextAccessor>().Object,
+            new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+            new Mock<IOptions<IdentityOptions>>().Object,
+            new Mock<ILogger<SignInManager<User>>>().Object,
+            new Mock<IAuthenticationSchemeProvider>().Object,
+            new Mock<IUserConfirmation<User>>().Object);
+        var mockServiceProvider = new Mock<IServiceProvider>();
+        var mockMapper = new Mock<IMapper>();
+        var mockRoleManager = MockHelpers.MockRoleManager<Role>();
+        _mockAuthenticationManager = new Mock<IJwtAuthManager>();
+        _controller = new UserController(_mockUserManager.Object,
+            _mockSignInManager.Object,
+            mockServiceProvider.Object,
+            mockMapper.Object, mockRoleManager.Object,
+            _mockAuthenticationManager.Object);
     }
     
     [Fact]
-    public async Task TestLogin_GoodResult()
+    public Task TestLogin_GoodResult()
     {
         var loginModel = new LoginModel
         {
@@ -48,181 +58,153 @@ public class UserControllerTests
             Password = "Administrator!1"
         };
         
-        mockSignInManager.Setup(x => x.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+        _mockSignInManager.Setup(x =>
+                x.PasswordSignInAsync(It.IsAny<string>(),
+                    It.IsAny<string>(), 
+                    It.IsAny<bool>(),
+                    It.IsAny<bool>()))
             .ReturnsAsync(SignInResult.Success);
         
         //Assert
-        var result = controller.Login(loginModel);
+        var result = _controller.Login(loginModel);
         Assert.NotNull(result.Result);
         Assert.IsType<OkObjectResult>(result.Result.Result); 
         Assert.IsNotType<UnauthorizedObjectResult>(result.Result.Result);
+        return Task.CompletedTask;
     }
     
-        [Fact]
-        public async Task TestLogin_InvalidEmail()
+    [Fact]
+    public async Task TestLogin_InvalidEmail()
+    {
+        // Arrange
+        var loginModel = new LoginModel
         {
-            // Arrange
-            var loginModel = new LoginModel
-            {
-                Email = "invalidemail@domain.com", 
-                Password = "Administrator!1"
-            };
-      
-            mockSignInManager.Setup(x => x.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
-                .ReturnsAsync(SignInResult.Failed); 
+            Email = "invalidemail@domain.com", 
+            Password = "Administrator!1"
+        };
+  
+        _mockSignInManager.Setup(x =>
+                x.PasswordSignInAsync(It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<bool>()))
+            .ReturnsAsync(SignInResult.Failed); 
 
-            // Act
-            var result = await controller.Login(loginModel);
+        // Act
+        var result = await _controller.Login(loginModel);
 
-            // Assert
-            Assert.NotNull(result.Result);  
-            Assert.IsType<UnauthorizedObjectResult>(result.Result);
-        }
+        // Assert
+        Assert.NotNull(result.Result);  
+        Assert.IsType<UnauthorizedObjectResult>(result.Result);
+    }
         
-
-        [Fact]
-        public async Task TestChangePassword_Success()
+    [Fact]
+    public async Task TestChangePassword_Success()
+    {
+        // Arrange
+        var existingUser = new User { Id = "1", Email = "admin@fh-joanneum.at" };
+        var newPassword = "NewPassword!2";
+        var changePasswordModel = new ChangePasswordModel
         {
-            // Arrange
-            var existingUser = new User { Id = "1", Email = "admin@fh-joanneum.at" };
-            var newPassword = "NewPassword!2";
-            var changePasswordModel = new ChangePasswordModel
-            {
-                OldPassword = "Administrator!1",
-                NewPassword = newPassword
-            };
+            OldPassword = "Administrator!1",
+            NewPassword = newPassword
+        };
 
-            var claimsPrincipal =
-                new ClaimsPrincipal(new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, "admin@fh-joanneum.at") },
-                    "mock"));
-
-            mockUserManager.Setup(x => x.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(existingUser);
-            mockUserManager.Setup(x => x.ChangePasswordAsync(existingUser, changePasswordModel.OldPassword, newPassword))
-                .ReturnsAsync(IdentityResult.Success);
-            mockUserManager.Setup(x => x.UpdateAsync(existingUser)).ReturnsAsync(IdentityResult.Success);
-
-            // Act
-            var result = await controller.ChangePassword(changePasswordModel);
-
-            // Assert
-            Assert.IsType<OkResult>(result);
-        }
-        
-        [Fact]
-        public async Task TestRefresh_Success()
-        {
-            // Arrange
-            var refreshRequest = new RefreshRequest { RefreshToken = "valid_refresh_token" };
-
-            var mockPrincipal = new Mock<ClaimsPrincipal>();
-            mockPrincipal.Setup(p => p.FindFirst(ClaimTypes.NameIdentifier)).Returns(new Claim(ClaimTypes.NameIdentifier, "user_id"));
-
-            mockAthManager.Setup(j => j.GetPrincipalFromToken("valid_refresh_token", true)).Returns(mockPrincipal.Object);
-            mockUserManager.Setup(um => um.FindByIdAsync("user_id")).ReturnsAsync(new User { Id = "user_id" });
-            mockAthManager.Setup(j => j.GenerateTokens(It.IsAny<User>(), It.IsAny<DateTime>())).ReturnsAsync(new JwtAuthResultViewModel());
-
-            // Act
-            var result = await controller.Refresh(refreshRequest);
-
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
-            var newTokens = Assert.IsType<JwtAuthResultViewModel>(okResult.Value);
-            Assert.NotNull(newTokens);
-        }
-
-        [Fact]
-        public async Task TestRefresh_InvalidToken()
-        {
-            // Arrange
-            var refreshRequest = new RefreshRequest { RefreshToken = "invalid_refresh_token" };
-            mockAthManager.Setup(j => j.GetPrincipalFromToken("invalid_refresh_token", true)).Returns(new ClaimsPrincipal());
-
-            // Act
-            var result = await controller.Refresh(refreshRequest);
-
-            // Assert
-            var unauthorizedResult = Assert.IsType<UnauthorizedObjectResult>(result);
-            Assert.Equal("Invalid token - no user ID", unauthorizedResult.Value);
-        }
-
-        [Fact]
-        public async Task TestRefresh_UserNotFound()
-        {
-            // Arrange
-            var refreshRequest = new RefreshRequest { RefreshToken = "valid_refresh_token" };
-
-            var mockPrincipal = new Mock<ClaimsPrincipal>();
-            mockPrincipal.Setup(p => p.FindFirst(ClaimTypes.NameIdentifier)).Returns(new Claim(ClaimTypes.NameIdentifier, "non_existing_user_id"));
-
-            mockAthManager.Setup(j => j.GetPrincipalFromToken("valid_refresh_token", true)).Returns(mockPrincipal.Object);
-            mockUserManager.Setup(um => um.FindByIdAsync("non_existing_user_id")).ReturnsAsync((User)null);
-
-            // Act
-            var result = await controller.Refresh(refreshRequest);
-
-            // Assert
-            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
-            Assert.Equal("User not found", notFoundResult.Value);
-        }
-
-
-            private void SetMockClaimsUser(User user)
-            {
-                var httpContext = new DefaultHttpContext
+        var claimsPrincipal =
+            new ClaimsPrincipal(new ClaimsIdentity(new[]
                 {
-                    User = new ClaimsPrincipal(new ClaimsIdentity(
-                    [
-                        new Claim(ClaimTypes.NameIdentifier, user.Id),
-                    ], "mock"))
-                };
-                controller.ControllerContext = new ControllerContext()
-                {
-                    HttpContext = httpContext,
-                };
-            }
+                    new Claim(ClaimTypes.Name, "admin@fh-joanneum.at")
+                },
+                "mock"));
 
-        }
+        _mockUserManager.Setup(x =>
+            x.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(existingUser);
+        _mockUserManager.Setup(x =>
+                x.ChangePasswordAsync(existingUser, changePasswordModel.OldPassword, newPassword))
+            .ReturnsAsync(IdentityResult.Success);
+        _mockUserManager.Setup(x =>
+            x.UpdateAsync(existingUser)).ReturnsAsync(IdentityResult.Success);
 
-public class FakeSignInManager : SignInManager<User>
-{
-    public FakeSignInManager()
-        : base(new FakeUserManager(),
-            new Mock<IHttpContextAccessor>().Object,
-            new Mock<IUserClaimsPrincipalFactory<User>>().Object,
-            new Mock<IOptions<IdentityOptions>>().Object,
-            new Mock<ILogger<SignInManager<User>>>().Object,
-            new Mock<IAuthenticationSchemeProvider>().Object,
-            new Mock<IUserConfirmation<User>>().Object)
-    { }        
-}
+        // Act
+        var result = await _controller.ChangePassword(changePasswordModel);
 
-public class FakeUserManager : UserManager<User>
-{
-    public FakeUserManager()
-        : base(new Mock<IUserStore<User>>().Object,
-            new Mock<IOptions<IdentityOptions>>().Object,
-            new Mock<IPasswordHasher<User>>().Object,
-            new IUserValidator<User>[0],
-            new IPasswordValidator<User>[0],
-            new Mock<ILookupNormalizer>().Object,
-            new Mock<IdentityErrorDescriber>().Object,
-            new Mock<IServiceProvider>().Object,
-            new Mock<ILogger<UserManager<User>>>().Object)
-    { }
-
-    public override Task<IdentityResult> CreateAsync(User user, string password)
+        // Assert
+        Assert.IsType<OkResult>(result);
+    }
+    
+    [Fact]
+    public async Task TestRefresh_Success()
     {
-        return Task.FromResult(IdentityResult.Success);
+        // Arrange
+        var refreshRequest = new RefreshRequest { RefreshToken = "valid_refresh_token" };
+
+        var mockPrincipal = new Mock<ClaimsPrincipal>();
+        mockPrincipal.Setup(p => p.FindFirst(ClaimTypes.NameIdentifier))
+            .Returns(new Claim(ClaimTypes.NameIdentifier, "user_id"));
+
+
+        _mockAuthenticationManager.Setup(j =>
+            j.GetPrincipalFromToken("valid_refresh_token", true))
+            .Returns(mockPrincipal.Object);
+
+        _mockUserManager.Setup(um =>
+            um.FindByIdAsync("user_id"))
+            .ReturnsAsync(new User { Id = "user_id" });
+
+        _mockAuthenticationManager.Setup(j =>
+            j.GenerateTokens(It.IsAny<User>(), It.IsAny<DateTime>()))
+            .ReturnsAsync(new JwtAuthResultViewModel());
+
+        // Act
+        var result = await _controller.Refresh(refreshRequest);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var newTokens = Assert.IsType<JwtAuthResultViewModel>(okResult.Value);
+        Assert.NotNull(newTokens);
     }
 
-    public override Task<IdentityResult> AddToRoleAsync(User user, string role)
+    [Fact]
+    public async Task TestRefresh_InvalidToken()
     {
-        return Task.FromResult(IdentityResult.Success);
+        // Arrange
+        var refreshRequest = new RefreshRequest { RefreshToken = "invalid_refresh_token" };
+        _mockAuthenticationManager.Setup(j =>
+            j.GetPrincipalFromToken("invalid_refresh_token", true))
+            .Returns(new ClaimsPrincipal());
+
+        // Act
+        var result = await _controller.Refresh(refreshRequest);
+
+        // Assert
+        var unauthorizedResult = Assert.IsType<UnauthorizedObjectResult>(result);
+        Assert.Equal("Invalid token - no user ID", unauthorizedResult.Value);
     }
 
-    public override Task<string> GenerateEmailConfirmationTokenAsync(User user)
+    [Fact]
+    public async Task TestRefresh_UserNotFound()
     {
-        return Task.FromResult(Guid.NewGuid().ToString());
-    }
+        // Arrange
+        var refreshRequest = new RefreshRequest { RefreshToken = "valid_refresh_token" };
 
+        var mockPrincipal = new Mock<ClaimsPrincipal>();
+
+        mockPrincipal.Setup(p =>
+            p.FindFirst(ClaimTypes.NameIdentifier))
+            .Returns(new Claim(ClaimTypes.NameIdentifier, "non_existing_user_id"));
+
+        _mockAuthenticationManager.Setup(j =>
+            j.GetPrincipalFromToken("valid_refresh_token", true))
+            .Returns(mockPrincipal.Object);
+
+        _mockUserManager.Setup(um =>
+            um.FindByIdAsync("non_existing_user_id")).ReturnsAsync((User)null);
+
+        // Act
+        var result = await _controller.Refresh(refreshRequest);
+
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal("User not found", notFoundResult.Value);
+    }
 }
