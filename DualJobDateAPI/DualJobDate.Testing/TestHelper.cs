900522839efefd332b49e4fa40108d7e71139759
rosaDennig <102235974+rosaDennig@users.noreply.github.com>
2024-04-23 10:58:59 +0200

    Test Solution & first Controller Tests (#35)
    * CompanyControllerTests: for this tests  login with a User is missing:  GetCompanies_InvalidRequest_ReturnsBadRequestResult; GetCompanies_ValidRequest_ReturnsOkResult
    
    * Tests for Update Company Activity
    
    * Test for AddOrUpdateCompanyDetails
    
    * Tests for AddOrUpdateCompanyDetails and GetCompany Activities
    
    * Add Cliams mock
    
    * GetCompanies fixed and new UserController Test
    
    * removed todo
    
    * Controller Tests update after merge
    
    * added List Build Artifacts, Publish Build Artifacts and Download Build Artifacts
    
    * updated branch to feature/test
    
    * tests
    
    * updated dotnet test
    
    * updated dotnet test
    
    * added Find Test Project
    
    * set working_directory in job test
    
    * new and updated Controller Tests
    
    * fix: remove setting working directory
    
    * wip: testing pipeline log working direcotry
    
    * wip: fix automatic tests
    
    * wip: automatic test output tree
    
    * wip: automatic tests
    
    * fix: automatic tests
    
    * Refactor Tests
    
    ---------
    
    Co-authored-by: haraldmandl <harald.mandl@edu.fh-joanneum.at>
    Co-authored-by: kfj16 <lukaslinzer0161@gmail.com>
    Co-authored-by: Philipp Moritz <philipp.moritz@edu.fh-joanneum.at>

---
+++ DualJobDateAPI/DualJobDate.Testing/TestHelper.cs
@@ -0,0 +1,17 @@
+using DualJobDate.BusinessObjects.Entities;
+using DualJobDate.BusinessObjects.Entities.Enum;
+
+namespace DualJobDate.Testing;
+
+public class TestHelper
+{
+    public static Company GetTestCompany()
+    {
+        return new Company() { Name = "TestCompany", IsActive = true, Id = 1, InstitutionId = 1, AcademicProgramId = 1, CompanyDetailsId = 1, UserId = "98aad5f7-1fdc-45a1-9015-fbbfaf79e351"};
+    }
+
+    public static User GetTestAdminUser()
+    {
+        return new User() { UserType = UserTypeEnum.Admin, Id = "98aad5f7-1fdc-45a1-9015-fbbfaf79e351", };
+    }
+}
